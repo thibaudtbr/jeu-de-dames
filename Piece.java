665fe125f7a1c98439ef39b25c33414c41e934d8
phraphael <raphael.phan@ynov.com>
2025-01-27 10:48:36 +0100

    Création de la classse Piece et de son constructeur

---
+++ Piece.java
@@ -0,0 +1,14 @@
+public class Piece {
+    public String couleur;
+    public int line;
+    public int column;
+    public String role;
+
+    public Piece(String couleur, int line, int column, String role) {
+        this.couleur = couleur;
+        this.line = line;
+        this.column = column;
+        this.role = role;
+    }
+    
+}
