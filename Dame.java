ae14a6afb108b106234ae635f86a12e5a9f9a5dc
phraphael <raphael.phan@ynov.com>
2025-01-27 14:54:56 +0100

    correction orthographique du constructeur

--- Dame.java
+++ Dame.java
@@ -1,7 +1,7 @@
 public class Dame extends Piece{
     
     public Dame (int couleur, int line, int column, int role) {
-        super(int couleur, int line, int column, int role);
+        super(couleur,line, column, role);
     }
 
     public boolean deplacementValide(Case caseDepart, Case caseArrivee, Plateau plateau) {
