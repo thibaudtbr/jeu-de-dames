9e5064706e5ac092273c7ef34fb96c79b714dc78
phraphael <raphael.phan@ynov.com>
2025-01-27 11:57:49 +0100

    création des getter de line, colonne, couleur, role et position

--- Piece.java
+++ Piece.java
@@ -7,7 +7,41 @@ public class Piece {
     public Piece(String couleur, int line, int column, String role) {
         this.couleur = couleur;
         this.line = line;
-        this.column = column;
-        this.role = role;
+        this.column = copublic class Piece {
+            public String couleur;
+            public int line;
+            public int column;
+            public String role;
+        
+            public Piece(String couleur, int line, int column, String role) {
+                this.couleur = couleur;
+                this.line = line;
+                this.column = column;
+                this.role = role;
+            }
+            
+            public int getCouleur() {
+                return couleur;
+            }
+        
+            public int getLine() {
+                return line;
+            }
+        
+            public int getColumn() {
+                return column;
+            }
+        
+            public int getRole() {
+                return couleur;
+            }
+        
+            public int[] getPosition () {
+                int[] position = {line, column};
+                return position;
+            }
+        
+        }
+        
     }
 }
