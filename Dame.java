1dd960cbf3156b254f1986fbdb10b9fc5e7a8976
phraphael <raphael.phan@ynov.com>
2025-01-27 13:44:45 +0100

    Ajout de 'extend Piece' pour que la classe dame herite de la classe Piece

--- Dame.java
+++ Dame.java
@@ -1,8 +1,9 @@
-public class Dame {
+public class Dame extends Piece{
     
     public Dame (int couleur, int line, int column, int role) {
         super(int couleur, int line, int column, int role);
     }
 
     public boolean deplacementValide(Case caseDepart, Case caseArrivee, Plateau plateau) {
+    }
 }
