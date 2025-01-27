51a5fa538bf46487421e9214d68285455dd82efa
phraphael <raphael.phan@ynov.com>
2025-01-27 13:14:50 +0100

    suppression de la méthode getposition, passage de couleur et role en int

--- Piece.java
+++ Piece.java
@@ -1,47 +1,29 @@
 public class Piece {
-    public String couleur;
+    public int couleur;
     public int line;
     public int column;
-    public String role;
+    public int role;
 
-    public Piece(String couleur, int line, int column, String role) {
+    public Piece(int couleur, int line, int column, int role) {
         this.couleur = couleur;
         this.line = line;
-        this.column = copublic class Piece {
-            public String couleur;
-            public int line;
-            public int column;
-            public String role;
-        
-            public Piece(String couleur, int line, int column, String role) {
-                this.couleur = couleur;
-                this.line = line;
-                this.column = column;
-                this.role = role;
-            }
-            
-            public int getCouleur() {
-                return couleur;
-            }
-        
-            public int getLine() {
-                return line;
-            }
-        
-            public int getColumn() {
-                return column;
-            }
-        
-            public int getRole() {
-                return couleur;
-            }
-        
-            public int[] getPosition () {
-                int[] position = {line, column};
-                return position;
-            }
-        
-        }
-        
+        this.column = column;
+        this.role = role;
+    }
+    
+    public int getCouleur() {
+        return couleur;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRole() {
+        return couleur;
     }
 }
